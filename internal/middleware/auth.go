9ad56e24c383a4a609c4ba1f63a36da69f9aed84
Golu Kumar Pal <golukpal@ssipmt.com>
2025-09-15 14:18:13 +0530

    Add .gitignore file and format auth middleware code

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,28 +1,29 @@
 package middleware
 
 import (
-    "os"
-    "github.com/gofiber/fiber/v2"
-    "github.com/golang-jwt/jwt/v4"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func Protected() fiber.Handler {
-    return func(c *fiber.Ctx) error {
-        cookie := c.Cookies("jwt")
+	return func(c *fiber.Ctx) error {
+		cookie := c.Cookies("jwt")
 
-        token, err := jwt.Parse(cookie, func(token *jwt.Token) (interface{}, error) {
-            return []byte(os.Getenv("JWT_SECRET")), nil
-        })
+		token, err := jwt.Parse(cookie, func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("JWT_SECRET")), nil
+		})
 
-        if err != nil || !token.Valid {
-            return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-                "message": "Unauthorized",
-            })
-        }
+		if err != nil || !token.Valid {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"message": "Unauthorized",
+			})
+		}
 
-        claims := token.Claims.(jwt.MapClaims)
-        c.Locals("user", claims)
+		claims := token.Claims.(jwt.MapClaims)
+		c.Locals("user", claims)
 
-        return c.Next()
-    }
+		return c.Next()
+	}
 }
